3acd474cc18c0d4b94f72ba528ec2dc45f4d14dd
刘建辉 <jianhui@yzyw-inc.com>
2018-12-08 00:01:25 +0800

    update

--- src/typings/interface.d.ts
+++ src/typings/interface.d.ts
@@ -1,11 +1,11 @@
 export interface ShopAttibutes {
   id: number | null;
-  profile_id: number;
+  profile_id?: number;
   name: string;
   address: string;
   is_head: string;
-  latitude: string; // 纬度
-  longitude: string; // 经度
+  latitude?: string; // 纬度
+  longitude?: string; // 经度
   avatar: string; // 店铺头像
   remark: string; // 备注
   created_at?: Date;
